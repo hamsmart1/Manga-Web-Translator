f12b0187b2cdaeaa171c9b8a9262b90b422b0d0b
Hammy Kun✅ <120108800+hamsmart1@users.noreply.github.com>
2026-05-29 16:11:10 +0700

    Update launcher script to bridge with PowerShell for Unicode path support

--- run_manga_translator.bat
+++ run_manga_translator.bat
@@ -1,21 +1,3 @@
 @echo off
-title Manga Web Translator - Launcher
-echo Searching for Google Chrome...
-
-set "CHROME_PATH="
-
-if exist "%ProgramFiles%\Google\Chrome\Application\chrome.exe" (
-    set "CHROME_PATH=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-) else if exist "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" (
-    set "CHROME_PATH=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
-) else if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
-    set "CHROME_PATH=%LocalAppData%\Google\Chrome\Application\chrome.exe"
-)
-
-if "%CHROME_PATH%"=="" (
-    echo [ERROR] Google Chrome was not found.
-    pause
-    exit /b 1
-)
-
-start "" "%CHROME_PATH%" --load-extension="%~dp0." --user-data-dir="%TEMP%\manga-translator-chrome-profile"
+cd /d "%~dp0"
+powershell -NoProfile -ExecutionPolicy Bypass -File ".\run_manga_translator.ps1"
